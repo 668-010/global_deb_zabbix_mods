235e2c44dde44d42a4d8e6b7a7ddcbfc03c2dc10
668-010 <911@668-010.ru>
2018-04-21 11:08:58 +0500

    CPU temp modificated

--- global_deb_zabbix_mods/CPU_temp/cpu.temp.sh
+++ global_deb_zabbix_mods/CPU_temp/cpu.temp.sh
@@ -1,1 +1,17 @@
-sensors | grep "Physical id 0" | awk '{print $ 4}' | cut -c 2-3
+#!/bin/bash
+sens=/usr/bin/sensors
+if $sens | grep "Physical id 0" > /dev/nul
+    then $sens | grep "Physical id 0" | awk '{print $ 4}' | cut -c 2-3
+    else
+        if $sens | grep "Package id 0" > /dev/nul
+            then $sens | grep "Package id 0" | awk '{print $ 4}' | cut -c 2-3
+            else
+                if $sens | grep "temp1" > /dev/nul
+                    then $sens | grep "temp1" -m1 | awk '{print $ 5}' | cut -c 2-3
+                    else
+                        if $sens | grep "core1" > /dev/nul
+                            then $sens | grep "Core 0" -m1 | awk '{print $ 6}' | cut -c 2-3
+                        fi
+                fi
+        fi
+fi
