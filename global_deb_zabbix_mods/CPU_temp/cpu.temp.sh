c33d2ac74e170acdd5c05e9377b4930c8ce7098f
668-010 <911@668-010.ru>
2018-04-21 13:22:14 +0500

    fix errors cpu temp

--- global_deb_zabbix_mods/CPU_temp/cpu.temp.sh
+++ global_deb_zabbix_mods/CPU_temp/cpu.temp.sh
@@ -7,10 +7,10 @@ if $sens | grep "Physical id 0" > /dev/nul
             then $sens | grep "Package id 0" | awk '{print $ 4}' | cut -c 2-3
             else
                 if $sens | grep "temp1" > /dev/nul
-                    then $sens | grep "temp1" -m1 | awk '{print $ 5}' | cut -c 2-3
+                    then $sens | grep "temp1" -m1 | awk '{print $ 2}' | cut -c 2-3
                     else
                         if $sens | grep "core1" > /dev/nul
-                            then $sens | grep "Core 0" -m1 | awk '{print $ 6}' | cut -c 2-3
+                            then $sens | grep "Core 0" -m1 | awk '{print $ 3}' | cut -c 2-3
                         fi
                 fi
         fi
